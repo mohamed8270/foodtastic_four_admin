6e7f3dad5a37b0af675626e1f167837d08f39bd6
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2025-02-15 12:10:40 +0530

    profile screen updated

--- lib/features/products/screens/profile_screen.dart
+++ lib/features/products/screens/profile_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:foodtastic_four_admin/utils/constants/pages.dart';
+import 'package:foodtastic_four_admin/utils/device/device_utility.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var size = FDeviceUtility.getDeviceSize(context);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 260, width: 260, child: Image.asset(FAppImg.saladImg, fit: BoxFit.cover, filterQuality: FilterQuality.high,),),
+            Text('Mohamed Ibrahim', style: Theme.of(context).textTheme.headlineSmall),
+            FAppSizes.spaceXs,
+            Text('ibrahimrasith@gmail.com', style: Theme.of(context).textTheme.labelMedium?.copyWith(color: FAppColor.fBlack.withOpacity(0.5))),
+            FAppSizes.spaceMd,
+            GestureDetector(
+              onTap: () {},
+              child: Container(height: size.height * 0.06, width: size.width * 0.8,
+              decoration: BoxDecoration(color: FAppColor.fGreen, borderRadius: BorderRadius.circular(40)),
+                alignment: Alignment.center,
+                child: Text(FAppText.logOutTxt, style: Theme.of(context).textTheme.labelMedium?.copyWith(color: FAppColor.fWhite),),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
