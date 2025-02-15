c5cee48e5fd9495572dd10ef09f72342bf25e052
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2025-02-15 12:11:56 +0530

    local storage [secure storage added] local_storage.dart

--- lib/utils/local_storage/local_storage.dart
+++ lib/utils/local_storage/local_storage.dart
@@ -1,8 +1,24 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:foodtastic_four_admin/utils/constants/pages.dart';
+import 'package:foodtastic_four_admin/utils/logging/logging.dart';
 
 class LocalStorage {
   LocalStorage._();
 
   static List<String> images = [FAppImg.startUp, FAppImg.chinese, FAppImg.beveragesImg, FAppImg.northIndian, FAppImg.southIndian];
   static List<String> innerText = [FAppText.startUps, FAppText.chinese, FAppText.chinese, FAppText.northIndian, FAppText.southIndian];
+
+  // store auth and other tokens
+  static Future<void> storeToken(String token) async {
+    FlutterSecureStorage storage = const FlutterSecureStorage();
+    await storage.write(key: 'authToken', value: token);
+  }
+
+  // get auth token
+  static Future<String?> token() async {
+    FlutterSecureStorage storage = const FlutterSecureStorage();
+    String? token = await storage.read(key: 'authToken');
+    AppLogger.info(token.toString());
+    return token;
+  }
 }
