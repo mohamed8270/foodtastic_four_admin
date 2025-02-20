9049c1b1b59ffb0f30fe2400f746ea679d6a3d45
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2025-02-20 16:59:44 +0530

    local data storage for token and counter id

--- lib/utils/local_storage/local_storage.dart
+++ lib/utils/local_storage/local_storage.dart
@@ -6,12 +6,24 @@ class LocalStorage {
   LocalStorage._();
 
   static List<String> images = [FAppImg.startUp, FAppImg.chinese, FAppImg.beveragesImg, FAppImg.northIndian, FAppImg.southIndian];
-  static List<String> innerText = [FAppText.startUps, FAppText.chinese, FAppText.chinese, FAppText.northIndian, FAppText.southIndian];
+  static List<String> innerText = [FAppText.startUps, FAppText.chinese, FAppText.beverages, FAppText.northIndian, FAppText.southIndian];
 
   // store auth and other tokens
   static Future<void> storeToken(String token) async {
     FlutterSecureStorage storage = const FlutterSecureStorage();
     await storage.write(key: 'authToken', value: token);
+  }
+
+  // store counter
+  static Future<void> storeCounter(String counter) async {
+    FlutterSecureStorage storage = const FlutterSecureStorage();
+    await storage.write(key: 'counterValue', value: counter);
+  }
+
+  static Future<String?> counter() async {
+    FlutterSecureStorage storage = const FlutterSecureStorage();
+    String? counter = await storage.read(key: 'counterValue');
+    return counter;
   }
 
   // get auth token
