0381f0996a32304b4bf64412800cbeb1563f7933
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2025-02-26 12:16:11 +0530

    user register model id added

--- lib/features/authentication/models/user_register_model.dart
+++ lib/features/authentication/models/user_register_model.dart
@@ -1,18 +1,17 @@
 class UserRegisterModel {
+  final String id;
   final String employeeId;
   final int counterId;
   final String fullName;
   final String email;
-  final String password;
 
-  UserRegisterModel({required this.employeeId, required this.counterId, required this.fullName, required this.email, required this.password});
+  UserRegisterModel({required this.id, required this.employeeId, required this.counterId, required this.fullName, required this.email});
 
-  factory UserRegisterModel.fromJson(Map<String, dynamic> json) {
-    return UserRegisterModel(
+  factory UserRegisterModel.fromJson(Map<String, dynamic> json) => UserRegisterModel(
+        id: json['_id'],
         employeeId: json['employeeId'],
         counterId: json['counterId'],
         fullName: json['fullName'],
         email: json['email'],
-        password: json['password']);
-  }
+  );
 }
