d0dd16aefb9bf9880b0a99fa2d33cec97995edba
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2025-02-26 12:12:28 +0530

    logging and event class name changed

--- lib/features/products/blocs/profile_data/profile_data_bloc.dart
+++ lib/features/products/blocs/profile_data/profile_data_bloc.dart
@@ -5,17 +5,19 @@ import 'package:foodtastic_four_admin/features/products/blocs/profile_data/profi
 import 'package:foodtastic_four_admin/features/products/blocs/profile_data/profile_data_state.dart';
 import 'package:foodtastic_four_admin/utils/http/dio_exception_handler.dart';
 import 'package:foodtastic_four_admin/utils/local_storage/local_storage.dart';
+import 'package:foodtastic_four_admin/utils/logging/logging.dart';
 
 class ProfileDataBloc extends Bloc<ProfileDataEvent, ProfileDataState> {
   final AuthRepository authRepository;
 
   ProfileDataBloc(this.authRepository) : super(ProfileDataInitial()) {
-    on<ProfileDataEvent>((event, emit) async {
+    on<ProfileData>((event, emit) async {
       emit(ProfileDataLoading());
       try {
         String? token = await LocalStorage.token();
         if(token != null) {
           final userProfileData = await authRepository.staffProfile('Bearer $token');
+          AppLogger.debug('This is email ID: ${userProfileData.email}');
           emit(ProfileDataLoaded(userRegisterModel: userProfileData));
         } else {
           emit(ProfileDataError(error: "Can't load the profile data"));
